d92dd5f53a44941f82af605b7d02a815dae5f434
David-Eze <unyadavideze@gmail.com>
2026-07-24 14:12:01 +0100

    mod

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const connectDB = require('./config/db.config');
 
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = [
+  'https://su-youth-camp.netlify.app',
+  'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: 'https://su-youth-camp.netlify.app'
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 dotenv.config();

69baa44e2d76ad56013bc15ad9962612df8dbebf
David-Eze <unyadavideze@gmail.com>
2026-07-24 08:50:51 +0100

    modified

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db.config');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://su-youth-camp.netlify.app'
 }));
 
 dotenv.config();
